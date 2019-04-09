6f119d14eb9d6dc7130b496b30ea8fb5957327c8
Robert-Paul Respicio <respicio.robertpaul@gmail.com>
2019-04-08 18:26:02 -0700

    finish table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import marked from 'marked';
 import './App.scss';
 
+marked.setOptions = ({ breaks: true, tables: true })
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toBeCompiled: placeholder
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({
+      toBeCompiled: e.target.value
+    })
+  }
+
+  createMarkup = () => {
+    return { __html: marked(this.state.toBeCompiled) };
+  };
+
   render() {
     return (
       <div className="App">
@@ -12,12 +32,12 @@ class App extends Component {
           <div className="row">
             <div className="col" id="editor">
               <h3>Editor</h3>
-              <textarea rows="25" cols="100">
+              <textarea type="text" rows="40" cols="100" onChange={this.onChange} value={this.state.toBeCompiled} >
               </textarea>
             </div>
             <div className="col">
               <h3>Previewer</h3>
-              <div id="preview">
+              <div id="preview" dangerouslySetInnerHTML={this.createMarkup()}>
               </div>
             </div>
           </div>
@@ -26,5 +46,54 @@ class App extends Component {
     );
   }
 }
+
+const placeholder =
+  `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
+
 
 export default App;
